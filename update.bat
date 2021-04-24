ab057ff04c8999843c53a0fa8dc9b58ddcb024ce
Bishoy Maurice <bishoy.maurice@valeo.com>
2021-04-24 22:08:23 +0200

    Add IaC Project

--- update.bat
+++ update.bat
@@ -1,1 +1,2 @@
-aws cloudformation update-stack --region=us-west-2 --stack-name %1 --template-body file://%2
+
+aws cloudformation update-stack --stack-name %1 --template-body file://%2.yaml  --parameters file://%2-parameters.json --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-west-2
